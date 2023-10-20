5d9358cc3b0a0a281bdef054afb7fbe275aca3bc
pron1x <lukas98z@web.de>
2023-10-20 14:59:05 +0200

    device/nvme: Add controller register mapping; Log controller version

--- src/device/storage/nvme/NvmeController.cpp
+++ src/device/storage/nvme/NvmeController.cpp
@@ -1,6 +1,8 @@
 #include "NvmeController.h"
 
 #include "kernel/log/Logger.h"
+#include "kernel/system/System.h"
+#include "kernel/service/MemoryService.h"
 #include "device/pci/Pci.h"
 #include "device/pci/PciDevice.h"
 
@@ -9,6 +11,37 @@ Kernel::Logger NvmeController::log = Kernel::Logger::get("NVME");
 
     NvmeController::NvmeController(const PciDevice &pciDevice) {
         log.info("Initializing controller [0x%04x:0x%04x]", pciDevice.getVendorId(), pciDevice.getDeviceId());
+
+        auto &memoryService = Kernel::System::getService<Kernel::MemoryService>();
+
+        //Enable Bus Master DMA and Memory Space Access
+        uint16_t command = pciDevice.readWord(Pci::COMMAND);
+        command |= Pci::Command::BUS_MASTER | Pci::Command::MEMORY_SPACE;
+        pciDevice.writeWord(Pci::COMMAND, command);
+
+        // Calculate size for memory mapping
+        uint32_t bar0 = pciDevice.readDoubleWord(Pci::BASE_ADDRESS_0);
+        pciDevice.writeDoubleWord(Pci::BASE_ADDRESS_0, 0xFFFFFFFF);
+        uint32_t size = pciDevice.readDoubleWord(Pci::BASE_ADDRESS_0) & 0xFFFFFFF0;
+        pciDevice.writeDoubleWord(Pci::BASE_ADDRESS_0, bar0);
+
+        size = ~size + 1;
+
+        // Get physical address from BAR0 + BAR1;
+        uint32_t bar1 = pciDevice.readDoubleWord(Pci::BASE_ADDRESS_1);
+        uint64_t physicalAddress = ((bar0 & 0xFFFFFFF0) + ((bar1 &0xFFFFFFFF) << 32));
+
+        // Map physical address to memory
+        void* virtualAddress = memoryService.mapIO(physicalAddress, size);
+
+        uint32_t* Version = reinterpret_cast<uint32_t*>(virtualAddress + 0x8); // Version Offset: 0x8;
+        uint32_t mjr, mnr, ter, ver;
+        ver = *Version;
+        mjr = (ver >> 16) & 0xFFFF;
+        mnr = (ver >> 8) & 0xFF;
+        ter = ver & 0xFF;
+
+        log.info("Controller Version: %d.%d.%d", mjr, mnr, ter);
     }
 
     void NvmeController::initializeAvailableControllers() {
