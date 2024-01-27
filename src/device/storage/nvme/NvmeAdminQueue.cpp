c358a4ff05edb41387cf182e40c584a9c274ae76
pron1x <lukas98z@web.de>
2024-01-28 00:03:21 +0100

    device/nvme: Add documentation to NvmeAdminQueue, improve logging

--- src/device/storage/nvme/NvmeAdminQueue.cpp
+++ src/device/storage/nvme/NvmeAdminQueue.cpp
@@ -18,7 +18,7 @@ namespace Device::Storage {
         nvme->setAdminQueueRegisters(queue->getSubmissionPhysicalAddress(), queue->getCompletionPhysicalAddress());
         nvme->registerQueueInterruptHandler(id, queue);
 
-        log.debug("Initialized Admin Queue with size %d.", size);
+        log.trace("Initialized Admin Queue with size %d.", size);
     };
 
     void NvmeAdminQueue::sendIdentifyCommand(void* physicalDataPtr, uint16_t cns, uint32_t nsid = 0) {
@@ -71,18 +71,14 @@ namespace Device::Storage {
         NvmeQueue::NvmeCompletionEntry* result = queue->waitUntilComplete(slot);
         uint8_t statusCode = result->DW3.SF & 0xFF;
         uint8_t statusCodeType = (result->DW3.SF >> 8) & 0b111;
-        uint8_t retryDelay = (result->DW3.SF >> 11) & 0b11;
-        uint8_t more = (result->DW3.SF >> 13) & 1;
-        uint8_t noRetry = (result->DW3.SF >> 14) & 1;
-        log.debug("Status Code: %x, Status Code Type: %x, Retry Delay: %x, More: %x, No Retry: %x", statusCode, statusCodeType, retryDelay, more, noRetry);
+        log.trace("[Attach Namespace %d] Status Code: %x, Status Code Type: %x", nsid, statusCode, statusCodeType);
         memoryService.freeUserMemory(controllerList);
     }
 
     NvmeQueue* NvmeAdminQueue::createNewQueue(uint16_t queueId, uint32_t size) {
-        // Create queue object
         NvmeQueue* ioqueue = new NvmeQueue(nvme, queueId, size);
         queue->lockQueue();
-        // Submit command to create completion queue
+        // Create completion queue first
         uint32_t slot = queue->getSubmissionSlotNumber();
         NvmeQueue::NvmeCommand* command = queue->getSubmissionEntry();
         command->CDW0.CID = slot;
@@ -91,6 +87,7 @@ namespace Device::Storage {
         command->CDW0.OPC = OPC_CREATE_IO_COMPLETION_QUEUE;
         // PRP Entry 1 contains base memory address, since we use physical contigous memory regions
         command->PRP1 = ioqueue->getCompletionPhysicalAddress();
+        command->PRP2 = 0;
         // DWORD10 contains the queue size and identifier
         command->CDW10 = (size << 16 | queueId );
         // DWORD11 contains interrupt vector, interrupt enable and physical contiguity information
@@ -100,7 +97,7 @@ namespace Device::Storage {
         queue->updateSubmissionTail();
         queue->waitUntilComplete(slot);
 
-        // Create command to create submission queue
+        // Then create the submission queue
         queue->lockQueue();
         slot = queue->getSubmissionSlotNumber();
         command = queue->getSubmissionEntry();
@@ -110,10 +107,11 @@ namespace Device::Storage {
         command->CDW0.OPC = OPC_CREATE_IO_SUBMISSION_QUEUE;
         // PRP Entry 1 contains base memory address, since we use physical contigous memory regions
         command->PRP1 = ioqueue->getSubmissionPhysicalAddress();
+        command->PRP2 = 0;
         // DWORD10 contains queue size and identifier
         command->CDW10 = (size << 16 | queueId );
         // DWORD11 contains completion queue identifier, queue priority and physical contiguity information
-        command->CDW11 = (queueId << 16 | 0b11 << 1 | 1); // 0b11 is low priority, field ignored since we do not use wighted round robin arbitration
+        command->CDW11 = (queueId << 16 | 0b11 << 1 | 1); // 0b11 is low priority, field ignored since we do not use weighted round robin arbitration
         // DWORD12 contains NVM Set information. Cleared to 0 to not associate queue with specific NVM Set
         command->CDW12 = 0;
 
@@ -123,7 +121,7 @@ namespace Device::Storage {
         queue->waitUntilComplete(slot);
         nvme->registerQueueInterruptHandler(queueId, ioqueue);
 
-        log.debug("I/O Queue [%d] created.", queueId);
+        log.trace("I/O Queue [%d] created.", queueId);
         return ioqueue;
     }
 
--- src/device/storage/nvme/NvmeAdminQueue.h
+++ src/device/storage/nvme/NvmeAdminQueue.h
@@ -15,28 +15,42 @@ namespace Device::Storage {
 
 namespace Device::Storage {
     namespace Nvme {
+    /**
+     * @brief Interface for Nvme Admin Commands. Configures the internal queue and controller registers in the init function.
+    */
     class NvmeAdminQueue {
         public:
         
+        /**
+         * @brief Initializes the Admin Queue. Creates the internal Queue Pair and sets the approprioate controller registers.
+         * @param nvmeController Pointer to the NVMe Controller the Admin Queue is created for
+         * @param size The amount of entry slots for the queues.
+        */
         void Init(NvmeController* nvmeController, uint32_t size);
 
         /**
-         * Send the identify command. Refer to NVMe Reference 1.4 section 5.15
+         * @brief Send the identify command. Refer to NVMe Reference 1.4 section 5.15
          * @param physicalDataPtr The physical memory address of a continous 4096 byte memory region
          * @param cns The CNS Value to use for the command. Refer to Section 5.15.1 - Figure 244
          * @param nsid The Namespace ID if required by the command.
         */
         void sendIdentifyCommand(void* physicalDataPtr, uint16_t cns, uint32_t nsid);
 
+        /**
+         * @brief Send the Namespace Attachment command.
+         * @param controllerId The NVM Subsystem unique ID of the Controller to attach
+         * @param nsid The ID of the namespace to attach
+        */
         void attachNamespace(uint16_t controllerId, uint32_t nsid);
 
+        /**
+         * @brief Creates a new Nvme I/O Queue Pair and registers it as I/O Queues.
+         * @param queueId The ID of the new Queue Pair. Can not be 0 and must be incremented for every new Queue Pair
+         * @param size The amount of slots in the queue.
+         * @returns The pointer to the new NvmeQueue object
+        */
         NvmeQueue* createNewQueue(uint16_t queueId, uint32_t size);
         
-        private:
-
-        
-        public:
-
         private:
         static Kernel::Logger log;
         NvmeController* nvme;
